72bb8da38a3e0adda1b6918716259cb2905926a7
vipcodestudio <avipsyaifulloh.business@gmail.com>
2025-07-05 19:36:00 +0700

    video-18: pagination

--- src/components/common/data-table.tsx
+++ src/components/common/data-table.tsx
@@ -8,15 +8,26 @@ import {
   TableHeader,
   TableRow,
 } from '../ui/table';
+import PaginationDataTable from './pagination-data-table';
 
 export default function DataTable({
   header,
   data,
   isLoading,
+  totalPages,
+  currentPage,
+  currentLimit,
+  onChangePage,
+  onChangeLimit,
 }: {
   header: string[];
   data: (string | ReactNode)[][];
   isLoading?: boolean;
+  totalPages: number;
+  currentPage: number;
+  currentLimit: number;
+  onChangePage: (page: number) => void;
+  onChangeLimit: (limit: number) => void;
 }) {
   return (
     <div className="w-full flex flex-col gap-4">
@@ -61,6 +72,18 @@ export default function DataTable({
           </TableBody>
         </Table>
       </Card>
+      <div className="flex items-center justify-between">
+        <div></div>
+        {totalPages > 1 && (
+          <div className="flex justify-end">
+            <PaginationDataTable
+              currentPage={currentPage}
+              onChangePage={onChangePage}
+              totalPages={totalPages}
+            />
+          </div>
+        )}
+      </div>
     </div>
   );
 }
