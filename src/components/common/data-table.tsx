3e5ffb669e5d2f3f5e6c1652105b9de41b991db4
vipcodestudio <avipsyaifulloh.business@gmail.com>
2025-07-05 20:03:12 +0700

    video-19: limit

--- src/components/common/data-table.tsx
+++ src/components/common/data-table.tsx
@@ -9,6 +9,17 @@ import {
   TableRow,
 } from '../ui/table';
 import PaginationDataTable from './pagination-data-table';
+import { Label } from '../ui/label';
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from '../ui/select';
+import { LIMIT_LISTS } from '@/constants/data-table-constant';
 
 export default function DataTable({
   header,
@@ -73,7 +84,27 @@ export default function DataTable({
         </Table>
       </Card>
       <div className="flex items-center justify-between">
-        <div></div>
+        <div className="flex items-center gap-2">
+          <Label>Limit</Label>
+          <Select
+            value={currentLimit.toString()}
+            onValueChange={(value) => onChangeLimit(Number(value))}
+          >
+            <SelectTrigger>
+              <SelectValue placeholder="Select Limit" />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectGroup>
+                <SelectLabel>Limit</SelectLabel>
+                {LIMIT_LISTS.map((limit) => (
+                  <SelectItem key={limit} value={limit.toString()}>
+                    {limit}
+                  </SelectItem>
+                ))}
+              </SelectGroup>
+            </SelectContent>
+          </Select>
+        </div>
         {totalPages > 1 && (
           <div className="flex justify-end">
             <PaginationDataTable
