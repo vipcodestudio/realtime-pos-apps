ed160af1ae2448fecc844c3367da938413ff4546
vipcodestudio <avipsyaifulloh.business@gmail.com>
2025-07-05 14:57:47 +0700

    video-12: sidebar menu

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,15 @@
 import { DarkmodeToggle } from '@/components/common/darkmode-toggle';
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
+import Link from 'next/link';
 
 export default function Home() {
   return (
-    <div>
-      <Input />
-      <Button className="bg-red-400 dark:bg-amber-300">Hello</Button>
-      <DarkmodeToggle />
+    <div className="bg-muted flex justify-center items-center h-screen flex-col space-y-4">
+      <h1 className="text-4xl font-semibold">Welcome Avip Syaifulloh</h1>
+      <Link href="/admin">
+        <Button className="bg-teal-500 text-white">Access Dashboard</Button>
+      </Link>
     </div>
   );
 }
