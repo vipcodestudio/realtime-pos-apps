3275403b137e947ce6f668f7d2ff9365cbf1192b
vipcodestudio <avipsyaifulloh.business@gmail.com>
2025-07-08 19:20:08 +0700

    video-36: delete table

---
+++ src/app/(dashboard)/admin/table/_components/dialog-delete-table.tsx
@@ -0,0 +1,53 @@
+import DialogDelete from '@/components/common/dialog-delete';
+import { startTransition, useActionState, useEffect } from 'react';
+import { deleteTable } from '../actions';
+import { INITIAL_STATE_ACTION } from '@/constants/general-constant';
+import { toast } from 'sonner';
+import { Table } from '@/validations/table-validation';
+
+export default function DialogDeleteTable({
+  open,
+  refetch,
+  currentData,
+  handleChangeAction,
+}: {
+  refetch: () => void;
+  currentData?: Table;
+  open: boolean;
+  handleChangeAction: (open: boolean) => void;
+}) {
+  const [deleteTableState, deleteTableAction, isPendingDeleteTable] =
+    useActionState(deleteTable, INITIAL_STATE_ACTION);
+
+  const onSubmit = () => {
+    const formData = new FormData();
+    formData.append('id', currentData!.id as string);
+    startTransition(() => {
+      deleteTableAction(formData);
+    });
+  };
+
+  useEffect(() => {
+    if (deleteTableState?.status === 'error') {
+      toast.error('Delete Table Failed', {
+        description: deleteTableState.errors?._form?.[0],
+      });
+    }
+
+    if (deleteTableState?.status === 'success') {
+      toast.success('Delete Table Success');
+      handleChangeAction?.(false);
+      refetch();
+    }
+  }, [deleteTableState]);
+
+  return (
+    <DialogDelete
+      open={open}
+      onOpenChange={handleChangeAction}
+      isLoading={isPendingDeleteTable}
+      onSubmit={onSubmit}
+      title="Table"
+    />
+  );
+}
