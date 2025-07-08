eddee7df7c1d84a0ac6f590dc163d905863ef48a
vipcodestudio <avipsyaifulloh.business@gmail.com>
2025-07-08 19:30:18 +0700

    video-37 setup order schema in supabase

---
+++ src/migrations/004-order.sql
@@ -0,0 +1,29 @@
+create table public.orders (
+  id serial not null,
+  order_id text,
+  customer_name text,
+  status text,
+  payment_url text,
+  table_id integer references tables on delete set null,
+  created_at timestamp with time zone default timezone('utc'::text, now()) not null,
+  updated_at timestamp with time zone default timezone('utc'::text, now()) not null,
+
+  primary key (id)
+);
+
+alter table public.orders enable row level security;
+
+create table public.orders_menus (
+  id serial not null,
+  order_id integer references orders on delete set null,
+  menu_id integer references menus on delete set null,
+  status text,
+  quantity integer,
+  notes text,
+  created_at timestamp with time zone default timezone('utc'::text, now()) not null,
+  updated_at timestamp with time zone default timezone('utc'::text, now()) not null,
+
+  primary key (id)
+);
+
+alter table public.orders_menus enable row level security;
