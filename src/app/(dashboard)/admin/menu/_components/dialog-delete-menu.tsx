c4dc41be34e61419417294cdbfa1ae2207ab3b66
vipcodestudio <avipsyaifulloh.business@gmail.com>
2025-07-08 17:12:39 +0700

    video-31: delete menu

---
+++ src/app/(dashboard)/admin/menu/_components/dialog-delete-menu.tsx
@@ -0,0 +1,54 @@
+import DialogDelete from '@/components/common/dialog-delete';
+import { startTransition, useActionState, useEffect } from 'react';
+import { deleteMenu } from '../actions';
+import { INITIAL_STATE_ACTION } from '@/constants/general-constant';
+import { toast } from 'sonner';
+import { Menu } from '@/validations/menu-validation';
+
+export default function DialogDeleteMenu({
+  open,
+  refetch,
+  currentData,
+  handleChangeAction,
+}: {
+  refetch: () => void;
+  currentData?: Menu;
+  open: boolean;
+  handleChangeAction: (open: boolean) => void;
+}) {
+  const [deleteMenuState, deleteMenuAction, isPendingDeleteMenu] =
+    useActionState(deleteMenu, INITIAL_STATE_ACTION);
+
+  const onSubmit = () => {
+    const formData = new FormData();
+    formData.append('id', currentData!.id as string);
+    formData.append('image_url', currentData!.image_url as string);
+    startTransition(() => {
+      deleteMenuAction(formData);
+    });
+  };
+
+  useEffect(() => {
+    if (deleteMenuState?.status === 'error') {
+      toast.error('Delete Menu Failed', {
+        description: deleteMenuState.errors?._form?.[0],
+      });
+    }
+
+    if (deleteMenuState?.status === 'success') {
+      toast.success('Delete Menu Success');
+      handleChangeAction?.(false);
+      refetch();
+    }
+  }, [deleteMenuState]);
+
+  return (
+    <DialogDelete
+      open={open}
+      onOpenChange={handleChangeAction}
+      isLoading={isPendingDeleteMenu}
+      onSubmit={onSubmit}
+      title="Menu"
+    />
+  );
+}
