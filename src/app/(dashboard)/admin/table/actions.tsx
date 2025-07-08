37f15016bc0d4a9d717c73cdcf178a9f80f52b87
vipcodestudio <avipsyaifulloh.business@gmail.com>
2025-07-08 19:05:28 +0700

    video-34: create table

---
+++ src/app/(dashboard)/admin/table/actions.tsx
@@ -0,0 +1,50 @@
+'use server';
+
+import { createClient } from '@/lib/supabase/server';
+import { TableFormState } from '@/types/table';
+import { tableSchema } from '@/validations/table-validation';
+
+export async function createTable(
+  prevState: TableFormState,
+  formData: FormData,
+) {
+  let validatedFields = tableSchema.safeParse({
+    name: formData.get('name'),
+    description: formData.get('description'),
+    capacity: parseInt(formData.get('capacity') as string),
+    status: formData.get('status'),
+  });
+
+  if (!validatedFields.success) {
+    return {
+      status: 'error',
+      errors: {
+        ...validatedFields.error.flatten().fieldErrors,
+        _form: [],
+      },
+    };
+  }
+
+  const supabase = await createClient();
+
+  const { error } = await supabase.from('tables').insert({
+    name: validatedFields.data.name,
+    description: validatedFields.data.description,
+    capacity: validatedFields.data.capacity,
+    status: validatedFields.data.status,
+  });
+
+  if (error) {
+    return {
+      status: 'error',
+      errors: {
+        ...prevState.errors,
+        _form: [error.message],
+      },
+    };
+  }
+
+  return {
+    status: 'success',
+  };
+}
