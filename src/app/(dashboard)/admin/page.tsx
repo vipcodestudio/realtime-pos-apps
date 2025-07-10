b178b6d3a32d97dd51dd7f7ea9305626cc46b38b
vipcodestudio <avipsyaifulloh.business@gmail.com>
2025-07-10 14:58:04 +0700

    video-52: dashboard

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,7 +1,9 @@
-export default function AdminDashboardPage() {
-  return (
-    <div>
-      <button>Dashboard</button>
-    </div>
-  );
+import Dashboard from './_components/dashboard';
+
+export const metadata = {
+  title: 'WPU Cafe | Dashboard',
+};
+
+export default function DashboardPage() {
+  return <Dashboard />;
 }
