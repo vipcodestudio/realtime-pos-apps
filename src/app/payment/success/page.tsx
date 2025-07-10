205aed8d39a547c1a813e199a0daeebd73cd2a40
vipcodestudio <avipsyaifulloh.business@gmail.com>
2025-07-10 14:03:13 +0700

    video-49: payment success & failed page

---
+++ src/app/payment/success/page.tsx
@@ -0,0 +1,9 @@
+import Success from './_components/success';
+
+export const metadata = {
+  title: 'WPU Cafe | Payment Success',
+};
+
+export default function SuccessPage() {
+  return <Success />;
+}
