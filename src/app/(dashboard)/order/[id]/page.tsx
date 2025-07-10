57c25f48156f3199b5d10c1a6d86e93ce09980e7
vipcodestudio <avipsyaifulloh.business@gmail.com>
2025-07-10 13:11:55 +0700

    video-47: setup midtrans

--- src/app/(dashboard)/order/[id]/page.tsx
+++ src/app/(dashboard)/order/[id]/page.tsx
@@ -1,4 +1,6 @@
+import Script from 'next/script';
 import DetailOrder from './_components/detail-order';
+import { environment } from '@/configs/environment';
 
 export const metadata = {
   title: 'WPU Cafe | Detail Order',
@@ -10,5 +12,14 @@ export default async function DetailOrderPage({
   params: Promise<{ id: string }>;
 }) {
   const { id } = await params;
-  return <DetailOrder id={id} />;
+  return (
+    <div className="w-full">
+      <Script
+        src={`${environment.MIDTRANS_API_URL}/snap/snap.js`}
+        data-client-key={environment.MIDTRANS_CLIENT_KEY}
+        strategy="lazyOnload"
+      />
+      <DetailOrder id={id} />
+    </div>
+  );
 }
